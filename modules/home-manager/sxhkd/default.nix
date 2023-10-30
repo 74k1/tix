0afbf753f8d0c740fa8c058de1d27c4958537850
74k1 <git.t@betsumei.com>
2023-10-30 18:22:31 +0100

    fix: idk, scrot still doesn't work

--- modules/home-manager/sxhkd/default.nix
+++ modules/home-manager/sxhkd/default.nix
@@ -10,7 +10,10 @@
       # open terminal
       "super + Return" = "bspc rule -a wezterm -o state=floating rectangle=1600x900x0x0 center=true && ${pkgs.wezterm}/bin/wezterm";
       # program launcher
-      "super + @space" = "${pkgs.rofi}/bin/rofi -config -no-lazy-grab -show drun -modi drun";
+      "super + space" = "${pkgs.rofi}/bin/rofi -config -no-lazy-grab -show drun -modi drun";
+
+      # scrot
+      "Print" = "exec --no-startup-id \"sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'\"";
 
       # close window
       "alt + F4" = "bspc node --close";
