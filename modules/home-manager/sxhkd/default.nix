b4519659e33fae62f604977b9ac62b9038a2b2d4
74k1 <git.t@betsumei.com>
2023-10-30 22:25:15 +0100

    um: idk

--- modules/home-manager/sxhkd/default.nix
+++ modules/home-manager/sxhkd/default.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }:
 
+let
+  mod = "super";
+  alt = "alt";
+in
 {
   services.sxhkd = {
     enable = true;
@@ -8,15 +12,18 @@
     # extraConfig = builtins.readFile ./sxhkdrc;
     keybindings = {
       # open terminal
-      "super + Return" = "bspc rule -a wezterm -o state=floating rectangle=1600x900x0x0 center=true && ${pkgs.wezterm}/bin/wezterm";
+      "${mod} + Return" = "bspc rule -a wezterm -o state=floating rectangle=1600x900x0x0 center=true && ${pkgs.wezterm}/bin/wezterm";
       # program launcher
-      "super + space" = "${pkgs.rofi}/bin/rofi -config -no-lazy-grab -show drun -modi drun";
+      "${mod} + space" = "${pkgs.rofi}/bin/rofi -config -no-lazy-grab -show drun -modi drun";
 
       # scrot
-      "Print" = "exec --no-startup-id \"sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'\"";
+      "Print" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'";
+      "${mod}+x" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'";
+      # Mac-like Keybind :^)
+      "${alt}+Shift+s" = "exec --no-startup-id \"sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'\"";
 
       # close window
-      "alt + F4" = "bspc node --close";
+      "${alt} + F4" = "bspc node --close";
     };
   };
 }
