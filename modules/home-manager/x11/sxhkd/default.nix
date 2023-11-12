3168486630bd90a017f34271ccd40d04f3e861c6
74k1 <git.t@betsumei.com>
2023-11-12 22:04:44 +0100

    wip-fix. play pause

--- modules/home-manager/x11/sxhkd/default.nix
+++ modules/home-manager/x11/sxhkd/default.nix
@@ -27,7 +27,7 @@ in
       "Print" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \"-i %i -g %g\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \"image/png\"'";
       "${mod} + x" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \"-i %i -g %g\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \"image/png\"'";
       # Mac-like Keybind :^)
-      "${alt} + Shift + s" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'";
+      "${alt} + shift + s" = "sh -e -c '${pkgs.shotgun}/bin/shotgun $(${pkgs.slop}/bin/slop -l -c 0.3,0.4,0.6,0.4 -f \\\\\"-i %i -g %g\\\\\") - | ${pkgs.xclip}/bin/xclip -selection clipboard -t \\\\\"image/png\\\\\"'";
 
       # close window
       "${alt} + F4" = "bspc node --close";
@@ -39,8 +39,8 @@ in
       #"XF86AudioMute" = "${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
       #"XF86MonBrightnessUp" = "${pkgs.xorg.xbacklight}/bin/xbacklight -inc 5";
       #"XF86MonBrightnessDown" = "${pkgs.xorg.xbacklight}/bin/xbacklight -dec 5";
-      #"XF86AudioPlay" = "${pkgs.playerctl}/bin/playerctl play-pause";
-      #"XF86AudioPause" = "${pkgs.playerctl}/bin/playerctl pause";
+      "XF86AudioPlay" = "${pkgs.playerctl}/bin/playerctl play-pause";
+      "XF86AudioPause" = "${pkgs.playerctl}/bin/playerctl pause";
       #"XF86AudioNext" = "${pkgs.playerctl}/bin/playerctl next";
       #"XF86AudioPrev" = "${pkgs.playerctl}/bin/playerctl previous";
 
@@ -50,8 +50,8 @@ in
       "XF86AudioMute" = "${pkgs.duvolbr}/bin/duvolbr vol_mute";
       #"XF86MonBrightnessUp" = "${pkgs.duvolbr}/bin/duvolbr bri_up";
       #"XF86MonBrightnessDown" = "${pkgs.duvolbr} bri_down";
-      "XF86AudioPlay" = "${pkgs.duvolbr}/bin/duvbolbr play_pause";
-      "XF86AudioPause" = "${pkgs.duvolbr}/bin/duvolbr play_pause";
+      #"XF86AudioPlay" = "${pkgs.duvolbr}/bin/duvbolbr play_pause";
+      #"XF86AudioPause" = "${pkgs.duvolbr}/bin/duvolbr play_pause";
       "XF86AudioNext" = "${pkgs.duvolbr}/bin/duvolbr next_track";
       "XF86AudioPrev" = "${pkgs.duvolbr}/bin/duvolbr prev_track";
     };
