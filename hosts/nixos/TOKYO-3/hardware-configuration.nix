6222775f52382ab54c7c74c2c5edf44ec464f286
74k1 <git.t@betsumei.com>
2024-01-21 16:24:46 +0100

    idk

--- hosts/nixos/TOKYO-3/hardware-configuration.nix
+++ hosts/nixos/TOKYO-3/hardware-configuration.nix
@@ -8,9 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
@@ -30,9 +30,10 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wg0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- modules/nixos/daemons/wireguard/default.nix
+++ modules/nixos/daemons/wireguard/default.nix
@@ -1,50 +1,60 @@
 { inputs, outputs, config, lib, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    wireguard-tools
-  ];
-
-  # NOTE: keygen
-  # umask 077
-  # wg genkey > key
-  # wg pubkey < key > key.pub
-
-  # networking.firewall.allowedUDPPorts = [];
-  systemd.network = {
-    netdevs = {
-      "50-wg0" = {
-        netdevConfig = {
-          Kind = "wireguard";
-          Name = "wg0";
-          MTUBytes = "1300";
-        };
-        wireguardConfig = {
-          PrivateKeyFile = "/home/taki/wg_private_key_secrest";
-          ListenPort = 51820;
-        };
-        wireguardPeers = {
-          lib.mapAttrsToList
-            (host: peerConfig: {
-              wireguardPeerConfig = peerConfig;
-            })
-            {
-              SERN = {
-                PublicKey = "QACsJg17ScDNt/fvV3WvBnGYZ7+YFaiIfamznBfE7Rw="
-                AllowedIPs = [
-                  "10.100.0.2/24"
-                  "0.0.0.0/0"
-                ];
-              };
-            };
-        };
-      };
+  services.dnsmasq.enable = true;
+  services.dnsmasq.extraConfig = ''
+    interface=wg0
+  '';
+  networking = {
+    nat = {
+      enable = true;
+      externalInterface = "enp0s31f6";
+      internalInterfaces = [ "wg0" ];
     };
-    networks.wg0 = {
-      matchConfig.Name = "wg0";
-      address = ["10.100.0.1/24"];
-      networkConfig = {
-        IPMasquerade = "ipv4";
-        IPForward = true;
+    firewall.allowedUDPPorts = [ 53 51820 ];
+    firewall.allowedTCPPorts = [ 53 ];
+    wg-quick.interfaces = {
+      wg0 = {
+        address = [ "10.100.0.1/24" ];
+        listenPort = 51820;
+        
+        postUp = ''
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp0s31f6 -j MASQUERADE
+        '';
+
+        postDown = ''
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        '';
+
+        privateKeyFile = "/home/taki/wg_private_key_secrets";
+
+        peers = [
+          { # SERN
+            publicKey = "QACsJg17ScDNt/fvV3WvBnGYZ7+YFaiIfamznBfE7Rw=";
+            allowedIPs = [
+              "10.100.0.2/24"
+              "0.0.0.0/0"
+              "::/0"
+            ];
+          }
+          { # MAGI
+            publicKey = "JOf290ilGAOw2msc6aQsE+oSjvLA1g9Cvw6RvmsTJj4=";
+            allowedIPs = [
+              "10.100.0.3/24"
+              "0.0.0.0/0"
+              "::/0"
+            ];
+          }
+          { # NERV
+            publicKey = "vnmW4+i/tKuiUx86JGOax3wHl1eAPwZj+/diVkpiZgM=";
+            allowedIPs = [
+              "10.100.0.4/24"
+              "0.0.0.0/0"
+              "::/0"
+            ];
+          }
+        ];
       };
     };
   };
