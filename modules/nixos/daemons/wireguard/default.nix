28a081e4aa7c798ac4b3bc56606b584e59e0f75e
74k1 <git.t@betsumei.com>
2024-01-27 15:07:32 +0100

    feat: comment

--- modules/nixos/daemons/wireguard/default.nix
+++ modules/nixos/daemons/wireguard/default.nix
@@ -1,30 +1,26 @@
 { inputs, outputs, config, lib, pkgs, ... }:
 {
-  services.dnsmasq.enable = true;
-  services.dnsmasq.extraConfig = ''
-    interface=wg0
-  '';
+  # if internet problems occur, check ip route lol
+  # sudo ip route del default dev wg0
+  # ^ this should do the trick.
   networking = {
     nat = {
       enable = true;
       externalInterface = "enp0s31f6";
       internalInterfaces = [ "wg0" ];
     };
-    firewall.allowedUDPPorts = [ 53 51820 ];
-    firewall.allowedTCPPorts = [ 53 ];
-    wg-quick.interfaces = {
+    firewall.allowedUDPPorts = [ 51820 ];
+    wireguard.interfaces = {
       wg0 = {
-        address = [ "10.100.0.1/24" ];
+        ips = [ "10.100.0.1/24" ];
         listenPort = 51820;
         
-        postUp = ''
-          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        postSetup = ''
           ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp0s31f6 -j MASQUERADE
         '';
 
-        postDown = ''
-          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
         '';
 
         privateKeyFile = "/home/taki/wg_private_key_secrets";
@@ -33,25 +29,22 @@
           { # SERN
             publicKey = "QACsJg17ScDNt/fvV3WvBnGYZ7+YFaiIfamznBfE7Rw=";
             allowedIPs = [
-              "10.100.0.2/24"
+              "10.100.0.2/32"
               "0.0.0.0/0"
-              "::/0"
             ];
           }
           { # MAGI
             publicKey = "JOf290ilGAOw2msc6aQsE+oSjvLA1g9Cvw6RvmsTJj4=";
             allowedIPs = [
-              "10.100.0.3/24"
+              "10.100.0.3/32"
               "0.0.0.0/0"
-              "::/0"
             ];
           }
           { # NERV
             publicKey = "vnmW4+i/tKuiUx86JGOax3wHl1eAPwZj+/diVkpiZgM=";
             allowedIPs = [
-              "10.100.0.4/24"
+              "10.100.0.4/32"
               "0.0.0.0/0"
-              "::/0"
             ];
           }
         ];
