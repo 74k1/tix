89a4cd23ce17b2a4a06e1fa7b0c06fdd3bb51bbb
74k1 <git.t@betsumei.com>
2024-01-28 17:19:15 +0100

    idk; i think it works?

--- modules/nixos/daemons/wireguard/default.nix
+++ modules/nixos/daemons/wireguard/default.nix
@@ -14,13 +14,13 @@
       wg0 = {
         ips = [ "10.100.0.1/24" ];
         listenPort = 51820;
-        
+
         postSetup = ''
           ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp0s31f6 -j MASQUERADE
         '';
 
         postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp0s31f6 -j MASQUERADE
         '';
 
         privateKeyFile = "/home/taki/wg_private_key_secrets";
@@ -30,21 +30,18 @@
             publicKey = "QACsJg17ScDNt/fvV3WvBnGYZ7+YFaiIfamznBfE7Rw=";
             allowedIPs = [
               "10.100.0.2/32"
-              "0.0.0.0/0"
             ];
           }
           { # MAGI
             publicKey = "JOf290ilGAOw2msc6aQsE+oSjvLA1g9Cvw6RvmsTJj4=";
             allowedIPs = [
               "10.100.0.3/32"
-              "0.0.0.0/0"
             ];
           }
           { # NERV
             publicKey = "vnmW4+i/tKuiUx86JGOax3wHl1eAPwZj+/diVkpiZgM=";
             allowedIPs = [
               "10.100.0.4/32"
-              "0.0.0.0/0"
             ];
           }
         ];
