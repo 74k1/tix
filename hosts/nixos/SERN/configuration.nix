eebd1a3755e27e769df89295b8845496258ae374
74k1 <git.t@betsumei.com>
2024-04-13 05:13:11 +0200

    feat: outline works !!

--- hosts/nixos/SERN/configuration.nix
+++ hosts/nixos/SERN/configuration.nix
@@ -281,6 +281,11 @@
           forceSSL = true;
           locations."/" = {
             proxyPass = "http://10.100.0.1:3030";
+            extraConfig = ''
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+            '';
           };
         };
         # "auth.example.com" = {
