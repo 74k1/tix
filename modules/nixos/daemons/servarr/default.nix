0b165499cc8f0104c3c937d85227fa6876902777
74k1 <git.t@betsumei.com>
2024-06-22 20:53:20 +0200

    fix/feat: Useful text

--- modules/nixos/daemons/servarr/default.nix
+++ modules/nixos/daemons/servarr/default.nix
@@ -17,6 +17,20 @@
   virtualisation.arion = {
     backend = "docker";
     projects = {
+      "homarr".settings.services."homarr".service = {
+        image = "ghcr.io/ajnart/homarr:latest";
+        restart = "unless-stopped";
+        volumes = [
+          # optional for docker integration
+          #"/var/run/docker.sock:/var/run/docker.sock"
+          "/var/lib/homarr/configs:/app/data/configs"
+          "/var/lib/homarr/icons:/app/public/icons"
+          "/var/lib/homarr/data:/data"
+        ];
+        ports = [
+          ''''${PORT:-7575}:7575''
+        ];
+      };
       "flaresolverr".settings.services."flaresolverr".service = {
         image = "ghcr.io/flaresolverr/flaresolverr:latest";
         restart = "unless-stopped";
