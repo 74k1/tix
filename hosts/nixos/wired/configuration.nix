d1c12bfd3cfc73f67c6a71cdaa075ad2d69972e7
74k1 <git.t@betsumei.com>
2024-07-01 18:57:14 +0200

    fix/feat:

--- hosts/nixos/wired/configuration.nix
+++ hosts/nixos/wired/configuration.nix
@@ -38,12 +38,12 @@
     xwayland.enable = true;
   };
 
-  environment.sessionVariables = {
+  # environment.sessionVariables = {
     # If cursor becomes invisible
-    WLR_NO_HARDWARE_CURSORS = "1";
+    # WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
-  };
+    # NIXOS_OZONE_WL = "1";
+  # };
 
   xdg.portal = {
     enable = true;
