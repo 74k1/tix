de72c03afe5f0d7e934b047ca987d448bd8d59b4
74k1 <git.t@betsumei.com>
2024-07-11 09:46:18 +0200

    fix/feat: fixed conflicts (LOL merge -s ours; push -f)

--- modules/nixos/taki/default.nix
+++ modules/nixos/taki/default.nix
@@ -1,20 +1,21 @@
 { inputs, outputs, lib, config, pkgs, ... }:
-let
+#let
   #berkeley-mono-typeface = pkgs.callPackage ../../pkgs/berkeley-mono-typeface;
-in
+#in
 {
   # define user account - don't forget to set password with `passwd`
   users.users.taki = {
     isNormalUser = true;
     description = "taki";
     extraGroups = [ "wheel" "networkmanager" "plex" ];
-    packages = with pkgs; [];
+    # packages = with pkgs; [];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmB5TLlaoy7JVpMYP0voMEQrGn2WGYapppxnQRD5JRS knights"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhjipcpqKCIRFK3o5QqqjGteAFEJdabnZqgraK2n8pa NERV"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF5VblfuasqhILQMzPNsJiEl4jVw+9HSa4rvH8ftHGZL MAGI"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhjipcpqKCIRFK3o5QqqjGteAFEJdabnZqgraK2n8pa psyche"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF5VblfuasqhILQMzPNsJiEl4jVw+9HSa4rvH8ftHGZL navi"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAixkN1qkHsQI752vzxubx/2cGpuQN+ZFbMUswC3lBga wired"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmsit4uJM5PL6HBiyHlC7fMhI9MvfK4fwVeK3lZnpjk EVA"
+      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmsit4uJM5PL6HBiyHlC7fMhI9MvfK4fwVeK3lZnpjk EVA"
+      "ssh-rsa blabla xyz@xyz"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVrGnNjwaQ8CL4IBKWx0Z3A+PFpy96r0t8e2zc7jRr+ eiri"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcSDZxE2I6ViR3oEMBGANuJeHqIUaq1MBYcRxokSOwR cyberia"
     ];
