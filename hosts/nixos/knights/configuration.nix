e10bc428f9820e9268060ab41c6c329c38dd72c8
74k1 <git.t@betsumei.com>
2024-07-19 18:55:47 +0200

    fix/feat: Refactored configuration.

--- hosts/nixos/knights/configuration.nix
+++ hosts/nixos/knights/configuration.nix
@@ -85,17 +85,17 @@
           enableACME = true;
           root = "/var/www/example.com/";
         };
-        "vault.example.com" = {
-          enableACME = true;
-          forceSSL = true;
-          locations."/" = {
-            proxyPass = "10.100.0.1:8222";
-            extraConfig = ''
-              proxy_http_version 1.1;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "upgrade";
-            '';
-          };
+        "vw.example.com" = {
+          #enableACME = true;
+          #forceSSL = true;
+          # locations."/" = {
+          #   proxyPass = "10.100.0.1:8222";
+          #   extraConfig = ''
+          #     proxy_http_version 1.1;
+          #     proxy_set_header Upgrade $http_upgrade;
+          #     proxy_set_header Connection "upgrade";
+          #   '';
+          # };
         };
         # "vw.example.com" = {
         #   enableACME = true;
