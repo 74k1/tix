3f05b1bc0215595c1a62b725983c17e71f773412
74k1 <git.t@betsumei.com>
2024-07-22 17:52:55 +0200

    fix/feat: perfect...

--- modules/nixos/daemons/outline/default.nix
+++ modules/nixos/daemons/outline/default.nix
@@ -11,6 +11,7 @@
     oidcAuthentication = {
       clientId = "354e1b80-d919-4596-ac13-0f42aa8a93a6";
       clientSecretFile = "/var/lib/outline/forgejo";
+      displayName = "Forgejo";
       authUrl = "https://git.example.com/login/oauth/authorize";
       tokenUrl = "https://git.example.com/login/oauth/access_token";
       userinfoUrl = "https://git.example.com/login/oauth/userinfo";
