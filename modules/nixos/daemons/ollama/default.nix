cc2e536effd8e4fa225c0328aca5c87603733b9c
74k1 <git.t@betsumei.com>
2024-07-27 23:54:31 +0200

    fix/feat: refuckulated the carbonator

--- modules/nixos/daemons/ollama/default.nix
+++ modules/nixos/daemons/ollama/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     host = "0.0.0.0";
     port = 11434;
-    acceleration = "rocm";
+    acceleration = "cuda";
     environmentVariables = {
       OLLAMA_ORIGINS = "*";
     };
