78dbf8821b982a3bd2d06e1cc632042b509e0a97
74k1 <git.t@betsumei.com>
2024-09-19 10:12:23 +0200

    fix/feat: bifurcation

--- modules/nixos/daemons/forgejo/default.nix
+++ modules/nixos/daemons/forgejo/default.nix
@@ -1,17 +1,42 @@
 { config, lib, pkgs, ... }:
 {
-  # GITEA
-  services.forgejo = {
-    enable = true;
-    settings = {
-      service = {
-        DISABLE_REGISTRATION = true;
-        ENABLE_REVERSE_PROXY_AUTHENTICATION = true;
+  # Git Server
+  services = {
+    forgejo = {
+      enable = true;
+      package = pkgs.forgejo;
+      settings = {
+        service = {
+          DISABLE_REGISTRATION = true;
+          ENABLE_REVERSE_PROXY_AUTHENTICATION = true;
+        };
+        server = {
+          DOMAIN = "git.example.com";
+          ROOT_URL = "https://git.example.com/";
+        };
+        actions = {
+          ENABLED = true;
+          DEFAULT_ACTIONS_URL = "github";
+        };
       };
-      server = {
-        DOMAIN = "git.example.com";
-        ROOT_URL = "https://git.example.com/";
+    };
+    # Runner
+    gitea-actions-runner = {
+      package = pkgs.forgejo-actions-runner;
+      instances.default = {
+        enable = true;
+        name = "monolith";
+        url = "https://git.example.com";
+        tokenFile = "/tmp/forgejo-runner-token";
+        labels = [
+          "ubuntu-latest:docker://node:16-bullseye"
+          "ubuntu-22.04:docker://node:16-bullseye"
+          "ubuntu-20.04:docker://node:16-bullseye"
+          "ubuntu-18.04:docker://node:16-buster"
+          # "native:host"
+        ];
       };
     };
   };
+
 }
