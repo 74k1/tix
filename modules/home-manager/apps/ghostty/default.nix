909409e19e9be22554938a7ec45b3b032ee17934
74k1 <git.t@betsumei.com>
2024-09-23 22:12:04 +0200

    fix/feat: Test commit. Please ignore

--- modules/home-manager/apps/ghostty/default.nix
+++ modules/home-manager/apps/ghostty/default.nix
@@ -6,8 +6,8 @@
   ...
 }:
 {
-  config.home = {
-    file.".config/ghostty/config".text = builtins.readFile ./cfg/config;
+  home = {
+    file.".config/ghostty/config".text = builtins.readFile ./config;
     packages = [
       inputs.ghostty.packages.x86_64-linux.default
     ];
