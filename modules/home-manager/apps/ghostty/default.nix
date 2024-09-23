05a82d59d60d746b5ba9c75d8820bf68c4babae1
74k1 <git.t@betsumei.com>
2024-09-23 19:16:50 +0200

    fix/feat: this is my quickfix branch and i will use to do my quickfixes

--- modules/home-manager/apps/ghostty/default.nix
+++ modules/home-manager/apps/ghostty/default.nix
@@ -6,8 +6,10 @@
   ...
 }:
 {
-  home.file.".config/ghostty/config".text = builtins.readFile ./cfg/config;
-  config.home.packages = [
-    inputs.ghostty.packages.x86_64-linux.default
-  ];
+  config.home = {
+    file.".config/ghostty/config".text = builtins.readFile ./cfg/config;
+    packages = [
+      inputs.ghostty.packages.x86_64-linux.default
+    ];
+  };
 }
