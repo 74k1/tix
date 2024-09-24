6c244fa2a97c0fc4c029135b7385a6483693bead
74k1 <git.t@betsumei.com>
2024-09-24 14:05:13 +0200

    fix/feat: Commit committed

--- modules/nixos/daemons/immich/default.nix
+++ modules/nixos/daemons/immich/default.nix
@@ -1,5 +1,4 @@
-# VVV we have access to flake from here
-{ inputs, config, lib, pkgs, ... }:
+{ inputs, outputs, config, lib, pkgs, ... }:
 {
   imports = [
     "${inputs.nixpkgs-master}/nixos/modules/services/web-apps/immich.nix"
@@ -8,11 +7,13 @@
   services.immich = {
     enable = true;
     # default would try to get it from pkgs
-    package = inputs.immich.legacyPackages.x86_64-linux.immich;
-    mediaLocation = "/mnt/btrfs_pools/immich_media/";
+    package = inputs.nixpkgs-master.outputs.legacyPackages.x86_64-linux.immich;
+    mediaLocation = "/mnt/btrfs_pool/immich_media/";
     # environment = '''';
     # secretsFile = /tmp/immich_secret; # als need to look at this now... try to do it without to see whar happen
     host = "0.0.0.0";
     port = 3001;
+    # takes too long to build. disabling AI
+    machine-learning.enable = false;
   };
 }
