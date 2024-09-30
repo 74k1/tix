367225872b9945717313dfd65d8af0c117f4d0a5
74k1 <git.t@betsumei.com>
2024-09-30 15:46:06 +0200

    fix/feat: Fingers crossed!

--- modules/nixos/daemons/immich/default.nix
+++ modules/nixos/daemons/immich/default.nix
@@ -1,13 +1,13 @@
 { inputs, outputs, config, lib, pkgs, ... }:
 {
-  imports = [
-    "${inputs.nixpkgs-master}/nixos/modules/services/web-apps/immich.nix"
-  ];
+  # imports = [
+  #   "${inputs.nixpkgs-master}/nixos/modules/services/web-apps/immich.nix"
+  # ];
 
   services.immich = {
     enable = true;
     # default would try to get it from pkgs
-    package = inputs.nixpkgs-master.outputs.legacyPackages.x86_64-linux.immich;
+    package = pkgs.immich; # inputs.nixpkgs-master.outputs.legacyPackages.x86_64-linux.immich;
     mediaLocation = "/mnt/btrfs_pool/immich_media/";
     # environment = '''';
     # secretsFile = /tmp/immich_secret; # als need to look at this now... try to do it without to see whar happen
