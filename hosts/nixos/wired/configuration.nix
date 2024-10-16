5a4f5800724b0946e80a474973d2e602c9c21047
74k1 <git.t@betsumei.com>
2024-10-16 20:28:27 +0200

    fix/feat: --help

--- hosts/nixos/wired/configuration.nix
+++ hosts/nixos/wired/configuration.nix
@@ -262,20 +262,20 @@
 
 
   hardware = {
-    # yeetmouse = {
-    #   enable = true;
-    #   parameters = {
-    #     AccelerationMode = "jump"; # looks most like motivity in Raw Accel
-    #     Sensitivity = 0.8;
-    #     # Offset = -15.0;
-    #     # PreScale = 0.20;
-    #     PreScale = 1.0;
-    #     Acceleration = 1.56;
-    #     Midpoint = 5.0;
-    #     UseSmoothing = true;
-    #     ScrollsPerTick = 1;
-    #   };
-    # };
+    yeetmouse = {
+      enable = true;
+      parameters = {
+        AccelerationMode = "jump"; # looks most like motivity in Raw Accel
+        Sensitivity = 0.8;
+        # Offset = -15.0;
+        # PreScale = 0.20;
+        PreScale = 1.0;
+        Acceleration = 1.56;
+        Midpoint = 5.0;
+        UseSmoothing = true;
+        ScrollsPerTick = 1;
+      };
+    };
     sane = {
       enable = true;
       brscan4 = {
