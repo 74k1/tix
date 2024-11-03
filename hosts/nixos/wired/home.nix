17ed1580c322c6fe19c3ab566a1d4b324626dd6b
74k1 <git.t@betsumei.com>
2024-11-03 16:17:43 +0100

    fix/feat: pushing another fix

--- hosts/nixos/wired/home.nix
+++ hosts/nixos/wired/home.nix
@@ -116,7 +116,23 @@
     plasticity
     # cura
     # curaengine_stable
-    orca-slicer
+    # orca-slicer
+    (orca-slicer.overrideAttrs (
+      final: prev: {
+        version = "2.2.0";
+        src = fetchFromGitHub {
+          owner = "SoftFever";
+          repo = "OrcaSlicer";
+          rev = "v${final.version}";
+          hash = "sha256-h+cHWhrp894KEbb3ic2N4fNTn13WlOSYoMsaof0RvRI=";
+        };
+        patches = builtins.filter (
+          p:
+          (builtins.baseNameOf p) != "0002-fix-build-for-gcc-13.diff"
+          && (builtins.baseNameOf p) != "meshboolean-const.patch"
+        ) prev.patches;
+      }
+    ))
 
     prismlauncher
     jdk17
