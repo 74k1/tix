1cced2a54137844955731aeb04590ca1810fbc0a
reo101 <pavel.atanasov2001@gmail.com>
2024-11-09 16:49:45 +0200

    feat(topology): add `legacyPackages` package for convenience
    Can now also build with (1) instead of just (2)
    - `nix build .#topology`
      (from `legacyPackages`, hidden in `nix flake show`)
    - `nix build .#topology.${system}.config.output`
      (raw flake output)

--- nix/topology.nix
+++ nix/topology.nix
@@ -7,6 +7,11 @@
   ];
 
   perSystem = { lib, pkgs, self', system, ... }: {
+    # NOTE: make you able to `nix build .#topology`
+    legacyPackages = {
+      topology = self.topology.${system}.config.output;
+    };
+
     topology = {
       nixosConfigurations = self.nixosConfigurations;
       modules = [
