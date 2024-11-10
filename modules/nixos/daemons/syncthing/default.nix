a885a9e896124159ff1190a2bffe006530144355
74k1 <git.t@betsumei.com>
2024-11-10 18:09:33 +0100

    feat(nixos): Add Syncthing daemon configuration with age secrets integration

--- modules/nixos/daemons/syncthing/default.nix
+++ modules/nixos/daemons/syncthing/default.nix
@@ -1,15 +1,36 @@
 { inputs, outputs, config, lib, pkgs, ... }:
 {
+  # age.secrets = {
+  #   "syncthing_cert" = {
+  #     rekeyFile = "${inputs.self}/secrets/syncthing_cert_pem";
+  #     # mode = "770";
+  #     owner = "syncthing";
+  #     group = "syncthing";
+  #   };
+  #   "syncthing_key" = {
+  #     rekeyFile = "${inputs.self}/secrets/syncthing_key_pem";
+  #     # mode = "770";
+  #     owner = "syncthing";
+  #     group = "syncthing";
+  #   };
+  # };
   services.syncthing = {
     enable = true;
+
+    # Declarative node IDs
+    # cert = config.age.secrets."syncthing_cert".path;
+    # key = config.age.secrets."syncthing_key".path;
+
     guiAddress = "0.0.0.0:8384";
     dataDir = "/mnt/btrfs_pool/syncthing_data";
     openDefaultPorts = true;
-    settings = {
-      options = {
-        relaysEnabled = true;
-        urAccepted = -1;
-      };
+    overrideDevices = false;
+    overrideFolders = false;
+    options = {
+      relaysEnabled = true;
+      urAccepted = -1;
     };
   };
+
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
 }
