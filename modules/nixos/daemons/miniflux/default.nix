5b0f37e3f1925b528d9ec9dca60073cb31e1501f
74k1 <git.t@betsumei.com>
2025-02-09 02:04:32 +0100

    fix(miniflux): use previous admin token

--- modules/nixos/daemons/miniflux/default.nix
+++ modules/nixos/daemons/miniflux/default.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, config, lib, pkgs, ... }:
 {
-  age.secrets."miniflux_secrets" = {
-    rekeyFile = "${inputs.self}/secrets/miniflux_secrets.age";
+  age.secrets."miniflux_admin" = {
+    rekeyFile = "${inputs.self}/secrets/miniflux_admin.age";
     # mode = "770";
     # owner = "nextcloud";
     # group = "nextcloud";
@@ -10,7 +10,7 @@
   services.miniflux = {
     enable = true;
     createDatabaseLocally = true;
-    adminCredentialsFile = config.age.secrets."miniflux_secrets".path;
+    adminCredentialsFile = config.age.secrets."miniflux_admin".path;
     config = {
       PORT = 8084;
       BASE_URL = "https://news.example.com/";
