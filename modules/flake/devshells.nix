3af5e9a9c96d48f1053dbcec11faf7cf2b8e44cd
74k1 <git.t@betsumei.com>
2025-03-30 17:10:47 +0200

    feat(devshells): remove unnecessary devshells

--- modules/flake/devshells.nix
+++ modules/flake/devshells.nix
@@ -6,21 +6,22 @@
       # `nix develop `
       default = pkgs.mkShell {
         buildInputs = [
-          pkgs.git
-          inputs'.agenix-rekey.packages.agenix-rekey
-          inputs'.deploy-rs.packages.deploy-rs
+          # pkgs.git
+          # inputs'.agenix-rekey.packages.agenix-rekey
+          # inputs'.deploy-rs.packages.deploy-rs
+          inputs'.rix101.packages.nix-enraged
         ];
       };
-      with-wireguard-tools = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          wireguard-tools
-        ];
-      };
-      macchina = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          macchina
-        ];
-      };
+      # with-wireguard-tools = pkgs.mkShell {
+      #   buildInputs = with pkgs; [
+      #     wireguard-tools
+      #   ];
+      # };
+      # macchina = pkgs.mkShell {
+      #   buildInputs = with pkgs; [
+      #     macchina
+      #   ];
+      # };
     };
   };
 }
