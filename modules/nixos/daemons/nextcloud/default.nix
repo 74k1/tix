832a51b74f00196020fcf15f18c4aedd7cdcc1de
74k1 <git.t@betsumei.com>
2025-03-30 17:10:47 +0200

    fix(nextcloud): annoying errors

--- modules/nixos/daemons/nextcloud/default.nix
+++ modules/nixos/daemons/nextcloud/default.nix
@@ -7,6 +7,10 @@
     group = "nextcloud";
   };
 
+  services.redis = {
+    enable = true;
+  };
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
@@ -19,7 +23,7 @@
     configureRedis = true;
     autoUpdateApps.enable = true;
     maxUploadSize = "100G";
-    fastcgiTimeout = 360;
+    fastcgiTimeout = 86400;
     config = {
       # adminpassFile = "/tmp/nextcloud_adminpass_secret";
       adminpassFile = config.age.secrets."nextcloud_admin".path;
@@ -29,12 +33,18 @@
     phpOptions = {
       "max_execution_time" = "600";
       "max_input_time" = "600";
-      "default_socket_timeout" = "360";
+      "request_terminate_timeout" = "600";
+      "default_socket_timeout" = "86400";
       "opcache.fast_shutdown" = "1";
       "opcache.interned_strings_buffer" = "8";
       "opcache.max_accelerated_files" = "10000";
       "opcache.memory_consumption" = "128";
       "opcache.revalidate_freq" = "1";
+      # "memcache.locking" = "\\OC\\Memcache\\Redis";
+      # "memcache.distributed" = "\\OC\\Memcache\\Redis";
+      # 504 error handling?
+      "php_admin_value[max_input_time]" = "86400";
+      "php_admin_value[max_execution_time]" = "86400";
     };
     settings = {
       trusted_proxies = [ "10.100.0.2" ];
@@ -53,6 +63,10 @@
         "OC\\Preview\\XBitmap"
         "OC\\Preview\\HEIC"
       ];
+      "bulkupload.enabled" = "false";
+      maintenance_window_start = "12";
+      preview_ffmpeg_path = "${pkgs.ffmpeg-headless}/bin/ffmpeg";
+      reduce_to_languages = [ "en" ];
     };
   };
 
