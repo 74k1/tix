9722e72309c0e06a8db3113005fc2d80d59933fa
74k1 <git.t@betsumei.com>
2025-03-30 17:10:47 +0200

    fix(fastfetch): set output modules

--- modules/home-manager/shell/fastfetch/default.nix
+++ modules/home-manager/shell/fastfetch/default.nix
@@ -18,6 +18,45 @@
         source = ./fastfetch_logo;
         type = "file";
       };
+      modules = [
+        "break"
+        "break"
+        "title"
+        "separator"
+        {
+          type = "os";
+          key = "Distro";
+          format = "{3}";
+        }
+        {
+          type = "kernel";
+          key = "Kernel Version";
+          format = "{2}";
+        }
+        {
+          type = "packages";
+          format = "{9}";
+        }
+        "shell"
+        "wm"
+        "separator"
+        {
+          type = "cpu";
+          format = "{1} - {3} Cores";
+        }
+        {
+          type = "gpu";
+          key = "GPU";
+          hideType = "integrated";
+          format = "{2}";
+        }
+        {
+          type = "memory";
+        }
+        {
+          type = "disk";
+        }
+      ];
     };
   };
 }
