1a17427c3959d9876c7979af12aa07b9ccf5794f
74k1 <git.t@betsumei.com>
2025-04-22 23:42:06 +0200

    feat(forgejo): add runners & mailer

--- modules/nixos/daemons/forgejo/default.nix
+++ modules/nixos/daemons/forgejo/default.nix
@@ -1,10 +1,10 @@
 { inputs, outputs, config, lib, pkgs, allSecrets, ... }:
 {
   age.secrets."forgejo_runner_token" = {
-    rekeyFile = "${inputs.self}/secrets/forgejo_runer_token.age";
-    mode = "770";
-    owner = "forgejo";
-    group = "forgejo";
+    rekeyFile = "${inputs.self}/secrets/forgejo_runner_token.age";
+    # mode = "770";
+    # owner = "forgejo";
+    # group = "forgejo";
   };
   # Git Server
   services = {
@@ -28,6 +28,15 @@
           BUILTIN_SSH_SERVER_USER = "forge";
           SSH_LISTEN_PORT = 2277;
         };
+        mailer = {
+          ENABLED = true;
+          PROTOCOL = "smtp+starttls";
+          SMTP_ADDR = allSecrets.global.mail.sender.host;
+          SMTP_PORT = allSecrets.global.mail.sender.port;
+          USER = allSecrets.global.mail.sender.username;
+          PASSWD = allSecrets.global.mail.sender.password;
+          FROM = "\"[SYSTEM] Forgejo\"<${allSecrets.global.mail.sender.username}>";
+        };
         actions = {
           ENABLED = true;
           DEFAULT_ACTIONS_URL = "github";
@@ -39,23 +48,23 @@
     };
     openssh.settings.AcceptEnv = "GIT_PROTOCOL";
     # Runner
-    # gitea-actions-runner = {
-    #   package = pkgs.forgejo-actions-runner;
-    #   instances.default = {
-    #     enable = true;
-    #     name = "monolith";
-    #     url = "https://git.${allSecrets.global.domain0}";
-    #     #tokenFile = "/tmp/forgejo-runner-token";
-    #     tokenFile = config.age.secrets."forgejo_runner_token".path;
-    #     labels = [
-    #       "ubuntu-latest:docker://node:16-bullseye"
-    #       "ubuntu-22.04:docker://node:16-bullseye"
-    #       "ubuntu-20.04:docker://node:16-bullseye"
-    #       "ubuntu-18.04:docker://node:16-buster"
-    #       # "native:host"
-    #     ];
-    #   };
-    # };
+    gitea-actions-runner = {
+      package = pkgs.forgejo-actions-runner;
+      instances.default = {
+        enable = true;
+        name = "monolith";
+        url = "https://git.${allSecrets.global.domain0}";
+        #tokenFile = "/tmp/forgejo-runner-token";
+        tokenFile = config.age.secrets."forgejo_runner_token".path;
+        labels = [
+          "ubuntu-latest:docker://node:16-bullseye"
+          "ubuntu-22.04:docker://node:16-bullseye"
+          "ubuntu-20.04:docker://node:16-bullseye"
+          "ubuntu-18.04:docker://node:16-buster"
+          # "native:host"
+        ];
+      };
+    };
   };
 
 }
