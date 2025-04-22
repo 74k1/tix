cc15d98f02dc0ca74d92f59318e2f94a262e12d3
74k1 <git.t@betsumei.com>
2025-04-22 23:42:06 +0200

    feat(n8n): enable

--- modules/nixos/daemons/n8n/default.nix
+++ modules/nixos/daemons/n8n/default.nix
@@ -1,13 +1,20 @@
 { config, lib, pkgs, ... }:
 {
-  services.n8n.enable = true;
-  environment.systemPackages = with pkgs; [ nodejs ];
-  
-  systemd.services.n8n = {
-    serviceConfig = {
-      Environment = [
-        "PATH=${lib.makeBinPath [ pkgs.nodejs pkgs.n8n ]}:\${PATH}"
-      ];
+  services.n8n = {
+    enable = true;
+    settings = {
+      listen_address = "0.0.0.0";
+      port = 5678;
     };
   };
+
+  # environment.systemPackages = with pkgs; [ nodejs ];
+  
+  # systemd.services.n8n = {
+  #   serviceConfig = {
+  #     Environment = [
+  #       "PATH=${lib.makeBinPath [ pkgs.nodejs pkgs.n8n ]}:\${PATH}"
+  #     ];
+  #   };
+  # };
 }
