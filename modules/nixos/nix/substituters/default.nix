5338b042ff9e93b9283354b73fdd651707cd3e86
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    feat(substituters): add garnix

--- modules/nixos/nix/substituters/default.nix
+++ modules/nixos/nix/substituters/default.nix
@@ -25,6 +25,10 @@
           public-key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
         }
         {
+          substituter = "https://cache.garnix.io";
+          public-key = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+        }
+        {
           substituter = "https://vicinae.cachix.org";
           public-key = "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=";
         }
