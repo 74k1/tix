583720bd2b8620832c6f13a6bf0bd2fb1eb923f2
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    chore(modules): formatting

--- modules/flake/modules.nix
+++ modules/flake/modules.nix
@@ -6,13 +6,10 @@
   withSystem,
   ...
 }:
-
 {
   flake = {
     nixosModules = import "${inputs.self}/modules/nixos";
-
     darwinModules = import "${inputs.self}/modules/darwin";
-
     homeManagerModules = import "${inputs.self}/modules/home-manager";
   };
 }
