589234abf7248278e5a9d042964f6ef29b04f81c
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    feat(devShells): use nix-enraged inside tix
    - for agenix stuff

--- modules/flake/devshells.nix
+++ modules/flake/devshells.nix
@@ -18,33 +18,12 @@
     }:
     {
       devShells = {
-        # usage:
-        # `nix develop <name>`
         default = pkgs.mkShell {
-          buildInputs = [
-            # pkgs.git
-            # inputs'.agenix-rekey.packages.agenix-rekey
-            # inputs'.deploy-rs.packages.deploy-rs
-            inputs'.rix101.packages.nix-enraged
-            # (inputs'.nixpkgs.legacyPackages.nh.override {
-            #   nix = inputs'.rix101.packages.nix-enraged;
-            # })
-            (inputs'.nixos-anywhere.packages.nixos-anywhere.override {
-              nix = inputs'.rix101.packages.nix-enraged;
-            })
-            (inputs'.nixpkgs.legacyPackages.nixos-rebuild.override {
-              nix = inputs'.rix101.packages.nix-enraged;
-            })
-            (inputs'.nixpkgs.legacyPackages.nixos-rebuild-ng.override {
-              nix = inputs'.rix101.packages.nix-enraged;
-            })
-          ];
+          # buildInputs = [ ];
+          shellHook = ''
+            export PATH="${inputs'.rix101.packages.nix-enraged}/bin:$PATH"
+          '';
         };
-        # with-macchina = pkgs.mkShell {
-        #   buildInputs = with pkgs; [
-        #     macchina
-        #   ];
-        # };
       };
     };
 }
