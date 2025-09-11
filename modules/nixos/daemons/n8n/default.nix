89e68f7a2689e97676ace313fde475b6e16125bd
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    feat(n8n): add more env vars

--- modules/nixos/daemons/n8n/default.nix
+++ modules/nixos/daemons/n8n/default.nix
@@ -1,6 +1,9 @@
 {
+  allSecrets,
   config,
+  inputs,
   lib,
+  outputs,
   pkgs,
   ...
 }:
@@ -8,8 +11,15 @@
   services.n8n = {
     enable = true;
     environment = {
+      GENERIC_TIMEZONE = "Europe/Zurich";
+      N8N_EDITOR_BASE_URL = "https://n8n.eiri.${allSecrets.global.domain01}/";
+      N8N_HOST = "n8n.eiri.${allSecrets.global.domain01}";
       N8N_LISTEN_ADDRESS = "0.0.0.0";
       N8N_PORT = 5678;
+      N8N_PROTOCOL = "https";
+      N8N_RESTRICT_FILE_ACCESS_TO = "/var/lib/n8n";
+      TZ = "Europe/Zurich";
+      WEBHOOK_URL = "https://n8n.eiri.${allSecrets.global.domain01}/";
     };
   };
 
