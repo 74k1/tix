c49254eb685642e2c01da04a73392f44b40457eb
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    chore(polybar): move to service

---
+++ modules/home-manager/wayland/polybar/default.nix
@@ -0,0 +1,40 @@
+{
+  inputs,
+  outputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  home.packages = [
+    # pkgs.material-symbols
+    # pkgs.material-icons
+  ];
+
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybar.override {
+      alsaSupport = true;
+      # githubSupport = true;
+      # mpdSupport = true;
+      pulseSupport = true;
+      # i3GapsSupport = true;
+    };
+    script = ''
+      for m in $(polybar --list-monitors | ${pkgs.coreutils}/bin/cut -d":" -f1); do
+        MONITOR=$m polybar --reload &
+      done
+    '';
+    config = {
+      "bar/main" = {
+        monitor = "\${env:MONITOR:}";
+      };
+      "module/pulse" = {
+        click-left = "${pkgs.pulseaudio}/bin/pactl -- set-sink-volume @DEFAULT_SINK@ 100%";
+        #click-right = "${pkgs.wezterm}/bin/wezterm -e ${pkgs.pulsemixer}/bin/pulsemixer";
+      };
+    };
+    extraConfig = builtins.readFile ./polybar.ini;
+  };
+}
