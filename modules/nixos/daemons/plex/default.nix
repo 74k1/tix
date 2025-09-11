22351fe34385362c3a0958a19634fada8da13127
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    chore(plex): pkgs.master -> pkgs.stable

--- modules/nixos/daemons/plex/default.nix
+++ modules/nixos/daemons/plex/default.nix
@@ -20,10 +20,30 @@
   services = {
     plex = {
       enable = true;
-      package = pkgs.master.plex;
+      package = pkgs.stable.plex;
       dataDir = "/var/lib/plex";
     };
 
     tautulli.enable = true;
   };
+
+  # systemd.services.plex.serviceConfig = let
+  #   pidFile = "${config.services.plex.dataDir}/Plex Media Server/plexmediaserver.pid";
+  # in {
+  #   KillSignal = lib.mkForce "SIGKILL";
+  #   Restart = lib.mkForce "no";
+  #   TimeoutStopSec = 10;
+  #   ExecStop = pkgs.writeShellScript "plex-stop" ''
+  #     ${pkgs.procps}/bin/pkill --signal 15 --pidfile "${pidFile}"
+  #
+  #     # Wait until plex service has been shutdown
+  #     # by checking if the PID file is gone
+  #     while [ -e "${pidFile}" ]; do
+  #       sleep 0.1
+  #     done
+  #
+  #     ${pkgs.coreutils}/bin/echo "Plex shutdown successful"
+  #   '';
+  #   PIDFile = lib.mkForce "";
+  # };
 }
