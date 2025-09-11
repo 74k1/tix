d077c6295a9bd9c363d88a6f53b5cde0a04e7b15
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    feat(forgejo): remove action runner for now

--- modules/nixos/daemons/forgejo/default.nix
+++ modules/nixos/daemons/forgejo/default.nix
@@ -56,23 +56,23 @@
     };
     openssh.settings.AcceptEnv = "GIT_PROTOCOL";
     # Runner
-    gitea-actions-runner = {
-      package = pkgs.forgejo-runner;
-      instances.default = {
-        enable = true;
-        name = "monolith";
-        url = "https://git.${allSecrets.global.domain0}";
-        #tokenFile = "/tmp/forgejo-runner-token";
-        tokenFile = config.age.secrets."forgejo_runner_token".path;
-        labels = [
-          "ubuntu-latest:docker://node:16-bullseye"
-          "ubuntu-22.04:docker://node:16-bullseye"
-          "ubuntu-20.04:docker://node:16-bullseye"
-          "ubuntu-18.04:docker://node:16-buster"
-          # "native:host"
-        ];
-      };
-    };
+    # gitea-actions-runner = {
+    #   package = pkgs.forgejo-runner;
+    #   instances.default = {
+    #     enable = true;
+    #     name = "monolith";
+    #     url = "https://git.${allSecrets.global.domain0}";
+    #     #tokenFile = "/tmp/forgejo-runner-token";
+    #     tokenFile = config.age.secrets."forgejo_runner_token".path;
+    #     labels = [
+    #       "ubuntu-latest:docker://node:16-bullseye"
+    #       "ubuntu-22.04:docker://node:16-bullseye"
+    #       "ubuntu-20.04:docker://node:16-bullseye"
+    #       "ubuntu-18.04:docker://node:16-buster"
+    #       # "native:host"
+    #     ];
+    #   };
+    # };
   };
 
 }
