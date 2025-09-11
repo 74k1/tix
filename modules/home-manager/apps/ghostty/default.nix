1699f7e77fd4bbd11fae7d3e7f6ac50f16488b4a
74k1 <git.t@betsumei.com>
2025-09-11 15:09:23 +0200

    feat(ghostty): config to nix

--- modules/home-manager/apps/ghostty/default.nix
+++ modules/home-manager/apps/ghostty/default.nix
@@ -6,10 +6,29 @@
   ...
 }:
 {
-  home = {
-    file.".config/ghostty/config".source = ./config;
-    packages = [
-      pkgs.master.ghostty
-    ];
+  programs.ghostty = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    # installBatSyntax = true;
+    # installVimSyntax = true;
+    package = pkgs.master.ghostty;
+    systemd.enable = true;
+    settings = {
+      cursor-style = "block";
+      font-family = [ "PP Supply Mono" "Sarasa Gothic SC" ];
+      font-size = 14.5;
+      background-opacity = 0.9;
+      shell-integration-features = "no-sudo,title";
+      keybind = [
+        "unconsumed:alt+d=scroll_page_fractional:0.5"
+        "unconsumed:alt+u=scroll_page_fractional:-0.5"
+        "ctrl+shift+1=increase_font_size:1"
+      ];
+      macos-titlebar-style = "tabs";
+      macos-secure-input-indication = true;
+      gtk-titlebar = false;
+    };
   };
 }
